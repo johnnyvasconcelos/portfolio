456321cccc799254660fc7e3271b22cc1533d344
Johnny Vasconcelos <91664887+johnnyvasconcelos@users.noreply.github.com>
2021-12-20 17:16:00 -0200

    Início

---
+++ script.js
@@ -0,0 +1,9 @@
+var options = {
+  strings: ['Web Designer...', 'Desenvolvedor Web...', 'Desenvolvedor Front End'],
+  typeSpeed: 85,
+  loop: true,
+  startDelay: 1000,
+  smartBackspace: true
+};
+
+var typed = new Typed('.element', options);
