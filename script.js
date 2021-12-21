a0320add474649a52692362caaeb33de97a68977
Johnny Vasconcelos <91664887+johnnyvasconcelos@users.noreply.github.com>
2021-12-21 15:47:56 -0200

    conclusão

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 var options = {
-  strings: [ 'Desenvolvedor Front End', 'Web Designer', 'Desenvolvedor Web'],
-  typeSpeed: 85,
+  strings: [ 'Desenvolvedor Front End', 'Web Designer', 'Desenvolvedor WordPress'],
   loop: true,
   startDelay: 1000,
+  typeSpeed: 85,
   smartBackspace: true
 };
 
 var typed = new Typed('.element', options);
+
+
+let menuMobile = document.querySelector('.mobile-menu')
+let menuButton = document.querySelector('.mobile-button')
+let bxx = document.querySelector('.bx-x')
+let itemMenu = document.querySelectorAll('.item')
+
+menuButton.addEventListener('click', menu)
+
+bxx.addEventListener('click', menu)
+itemMenu[0].addEventListener('click', menu)
+itemMenu[1].addEventListener('click', menu)
+itemMenu[2].addEventListener('click', menu)
+itemMenu[3].addEventListener('click', menu)
+itemMenu[4].addEventListener('click', menu)
+itemMenu[5].addEventListener('click', menu)
+
+function menu() {
+  menuMobile.classList.toggle('show')
+}
